051966c50211f73bf7816e47c790eb34dc2237a2
Geovane Souza <Geovanesouza33@gmail.com>
2020-04-09 21:00:32 -0300

    alterando classe de conexaoDB

--- src/main/java/br/senac/sp/db/ConexaoDB.java
+++ src/main/java/br/senac/sp/db/ConexaoDB.java
@@ -13,19 +13,39 @@ import java.util.logging.Logger;
 
 public class ConexaoDB {
 
+    private static String STATUS = "Não conectado";
+    private static String DRIVER = "com.mysql.cj.jdbc.Driver";  //A partir da versao 8.0, mudou para com.mysql.cj.jdbc.Driver (Connector/J)                   
+
+    private static String SERVER = "localhost";
+    private static String DATABASE = "notestore";              //nome do seu banco de dados
+    private static String PORT = "3305";
+
+    private static String USER = "root";                     //nome de um usuário de seu BD      
+    private static String PASSWORD = "";                     //sua senha de acesso
+
+    private static String URL = "";
+
+    private static Connection CONEXAO = null;
+
     static {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName(DRIVER);
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(ConexaoDB.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-    public static Connection getConexao() throws SQLException {
-        String dbURL = "jdbc:mysql://localhost:3305/notestore?useTimezone=true&serverTimezone=UTC&useSSL=false";
-        String user = "root";
-        String password = "";
-        return DriverManager.getConnection(dbURL, user, password);
+    public static Connection getConexao() {
+        String dbURL = "jdbc:mysql://" + SERVER + ":" + PORT + "/" + DATABASE + "?useTimezone=true&serverTimezone=UTC&useSSL=false";
+
+        try {
+            CONEXAO = DriverManager.getConnection(dbURL, USER, PASSWORD);
+        } catch (SQLException ex) {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+        return CONEXAO;
     }
 
 }
