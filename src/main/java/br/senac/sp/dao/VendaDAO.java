f4f9a57b652e0afc7e63fc0fab1f263670c71695
Wellington-Andrade <wellingtonevangelista28@gmail.com>
2020-05-03 14:42:01 -0300

    Atualizacao-Servlet-Inclusao-realizarVenda.js

--- src/main/java/br/senac/sp/dao/VendaDAO.java
+++ src/main/java/br/senac/sp/dao/VendaDAO.java
@@ -7,16 +7,14 @@ package br.senac.sp.dao;
 
 import br.senac.sp.db.ConexaoDB;
 import br.senac.sp.entidade.Cliente;
-import br.senac.sp.entidade.Venda;
 import br.senac.sp.entidade.Produto;
+import br.senac.sp.entidade.Venda;
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  *
@@ -27,7 +25,7 @@ public class VendaDAO {
     private final ProdutoDAO produtoDAO = new ProdutoDAO();
     private final ClienteDAO clienteDAO = new ClienteDAO();
 
-    private void atualizarQuantidadeProdutos(Venda venda) {
+    private void atualizarQuantidadeProdutos(Venda venda, Connection conexao) {
 
         for (Produto produto : venda.getProdutos()) {
 
@@ -38,11 +36,9 @@ public class VendaDAO {
 
     }
 
-    private void atualizarProdutoVenda(Venda venda) {
+    private void atualizarProdutoVenda(Venda venda, Connection conexao) {
 
         PreparedStatement ps = null;
-
-        Connection conexao = ConexaoDB.getConexao();
 
         try {
 
@@ -65,168 +61,22 @@ public class VendaDAO {
 
     }
 
-    /**
-     *
-     * @param venda
-     * @return true: venda realizar com sucesso, false: erro ao realivar venda
-     */
-    public boolean salvarVenda(Venda venda) {
-        Connection conexao = null;
-        PreparedStatement instrucaoSQL = null;
+    private void adicionaClienteVenda(Venda venda, int clienteId, Connection conexao) {
+        ArrayList<Cliente> clientes = clienteDAO.consultarCliente("", "");
+        Cliente saveCli = null;
 
-        try {
-
-            //Tenta estabeler a conexão com o SGBD e cria comando a ser executado conexão
-            //Obs: A classe GerenciadorConexao já carrega o Driver e define os parâmetros de conexão
-            conexao = ConexaoDB.getConexao();
-
-            instrucaoSQL = conexao.prepareStatement("INSERT INTO venda VALUES(default, ?, ?, ?, ?)",
-                    Statement.RETURN_GENERATED_KEYS);  //Caso queira retornar o ID do cliente
-
-            //Adiciono os parâmetros ao meu comando SQL
-            instrucaoSQL.setDate(1, new java.sql.Date(venda.getDataVenda().getTime()));
-            instrucaoSQL.setDouble(2, venda.getTotal());
-            instrucaoSQL.setInt(3, venda.getCliente().getId());
-            System.out.println(venda.getCliente().getId());
-            instrucaoSQL.setInt(4, venda.getFilial().getId());
-
-            int linhasAfetadas = instrucaoSQL.executeUpdate(); //sintetico executeQuery
-
-            if (linhasAfetadas > 0) {
-
-                ResultSet generatedKeys = instrucaoSQL.getGeneratedKeys(); //Recupero o ID do cliente
-                if (generatedKeys.next()) {
-                    venda.setId(generatedKeys.getInt(1));
-                    atualizarQuantidadeProdutos(venda);
-                    atualizarProdutoVenda(venda);
-                    return true;
-                } else {
-                    throw new SQLException("Falha ao obter o ID do cliente.");
-                }
-            } else {
-                return false;
-            }
-
-        } catch (SQLException ex) {
-            System.out.println(ex.getMessage());
-            return false;
-        } finally {
-
-            //Libero os recursos da memória
-            try {
-                if (instrucaoSQL != null) {
-                    instrucaoSQL.close();
-                }
-
-                ConexaoDB.fecharConexao();
-                conexao.close();
-
-            } catch (SQLException ex) {
+        for (Cliente c : clientes) {
+            if (c.getId() == clienteId) {
+                saveCli = c;
+                break;
             }
         }
-    }
-
-    private void adicionaClienteVenda(Venda venda, int clienteId) {
-
-        ArrayList<Cliente> cliente = clienteDAO.consultarCliente("ID", clienteId + "");
-
-        venda.setCliente(cliente.get(0));
-    }
-
-    /**
-     * Busca uma List do tipo Venda obtida do banco de dados
-     *
-     * @param inicio
-     * @param fim
-     * @return
-     */
-    public List<Venda> consultarVendas(java.util.Date inicio, java.util.Date fim) {
-        ResultSet rs = null;
-        Connection conexao;
-        PreparedStatement instrucaoSQL = null;
-
-        ArrayList<Venda> listaVenda = new ArrayList<>();
-
-        try {
-
-            conexao = ConexaoDB.getConexao();
-            instrucaoSQL = conexao.prepareStatement("SELECT * FROM venda WHERE data_venda BETWEEN ? and ?;");
-            instrucaoSQL.setDate(1, new Date(inicio.getTime()));
-            instrucaoSQL.setDate(2, new Date(fim.getTime()));
-            System.out.println("teste de data, remover print depois");
-
-            rs = instrucaoSQL.executeQuery();
-
-            while (rs.next()) {
-                Venda venda = new Venda();
-                venda.setDataVenda(rs.getDate("data_venda"));
-                venda.setTotal(rs.getInt("total_venda"));
-                venda.setId(rs.getInt("id"));
-
-                adicionaClienteVenda(venda, rs.getInt("id_cliente"));
-                adicionaProdutosNaVenda(venda, conexao);
-
-                listaVenda.add(venda);
-            }
-
-        } catch (SQLException ex) {
-            System.out.println(ex.getMessage());
-        } finally {
-            //Libero os recursos da memória
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-                if (instrucaoSQL != null) {
-                    instrucaoSQL.close();
-                }
-
-                ConexaoDB.fecharConexao();
-
-            } catch (SQLException ex) {
-            }
+        if (saveCli != null) {
+            venda.setCliente(saveCli);
+        }else{
+            System.out.println("Erro ao adicionar cliente na venda");
         }
 
-        return listaVenda;
-    }
-
-    private void adicionaProdutosNaVenda(Venda venda, Connection conexao) {
-
-        ResultSet rs = null;
-        PreparedStatement instrucaoSQL = null;
-
-        try {
-            instrucaoSQL = conexao.prepareStatement(
-                    "SELECT"
-                    + "    venda_produto.quantidade_produto AS quantidade_produto_na_venda,"
-                    + "    produto.ID_produto  AS id_produto,"
-                    + "    produto.nome_produto  AS nome_produto,"
-                    + "    produto.quantidade_produto AS quantidade_produto,"
-                    + "    produto.valor_produto  AS valor_produto"
-                    + "FROM"
-                    + "    venda_produto"
-                    + "        INNER JOIN"
-                    + "    produto ON produto.ID_produto = venda_produto.id_produto"
-                    + "WHERE"
-                    + "    id_venda = ?;");
-
-            instrucaoSQL.setInt(1, venda.getId());
-
-            rs = instrucaoSQL.executeQuery();
-
-            while (rs.next()) {
-                Produto produto = new Produto();
-                produto.setId(rs.getInt("id_produto"));
-                produto.setNome(rs.getString("nome_produto"));
-                produto.setQuantidade(rs.getInt("quantidade_produto"));
-                produto.setQuantidadeNaVenda(rs.getInt("quantidade_produto_na_venda"));
-                produto.setValor(rs.getDouble("valor_produto"));
-                venda.addProduto(produto);
-            }
-
-        } catch (SQLException ex) {
-            System.out.println(ex.getMessage());
-        }
     }
 
 }
--- src/main/java/br/senac/sp/entidade/Venda.java
+++ src/main/java/br/senac/sp/entidade/Venda.java
@@ -14,15 +14,55 @@ import java.util.List;
  * @author Wellington
  */
 public class Venda {
+    
     private int id;
     private Date dataVenda;
     private double total;
     private Cliente cliente;
-    private Filial filial;
     private List<Produto> produtos = new ArrayList<>();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getDataVenda() {
+        return dataVenda;
+    }
+
+    public void setDataVenda(Date dataVenda) {
+        this.dataVenda = dataVenda;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
     
- 
     public void addProduto(Produto produto){
+        
         int index = produtos.indexOf(produto);
         if(index == -1){ //Produto ainda não está na lista, então adiciona.
             produtos.add(new Produto(produto));
@@ -32,104 +72,26 @@ public class Venda {
             int quantidadeAtualizada = quantidadeAtual + produto.getQuantidadeNaVenda();
             encontrado.setQuantidadeNaVenda(quantidadeAtualizada);
         }
+        
         total = calculaTotal();
+        
     }
     
     private double calculaTotal(){
+        
         double soma = 0;
         for (int i = 0; i < produtos.size(); i++) {
             Produto produto = produtos.get(i);
-            soma = soma + (produto.getValor() * produto.getQuantidadeNaVenda());
+            soma = soma + (produto.getValor()* produto.getQuantidadeNaVenda());
         }
         return soma;
     }
     
     public void removerProduto(Produto produto){
+        
         produtos.remove(produto);
         total = calculaTotal();
+        
     }
-
-    /**
-     * @return id
-     */
-    public int getId() {
-        return id;
-    }
-
-    /**
-     * @param id id para set
-     */
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    /**
-     * @return dataVenda
-     */
-    public Date getDataVenda() {
-        return dataVenda;
-    }
-
-    /**
-     * @param dataVenda dataVenda para set
-     */
-    public void setDataVenda(Date dataVenda) {
-        this.dataVenda = dataVenda;
-    }
-
-    /**
-     * @return total
-     */
-    public double getTotal() {
-        return total;
-    }
-
-    /**
-     * @param total total para set
-     */
-    public void setTotal(double total) {
-        this.total = total;
-    }
-
-    /**
-     * @return cliente
-     */
-    public Cliente getCliente() {
-        return cliente;
-    }
-
-    /**
-     * @param cliente cliente para set
-     */
-    public void setCliente(Cliente cliente) {
-        this.cliente = cliente;
-    }
-
-    /**
-     * @return filial
-     */
-    public Filial getFilial() {
-        return filial;
-    }
-
-    /**
-     * @param filial filial para set
-     */
-    public void setFilial(Filial filial) {
-        this.filial = filial;
-    }
-
-    /**
-     * @return produtos
-     */
-    public List<Produto> getProdutos() {
-        return produtos;
-    }
-
-    /**
-     * @param produtos produtos para set
-     */
-    public void setProdutos(List<Produto> produtos) {
-        this.produtos = produtos;
-    }
+    
 }
